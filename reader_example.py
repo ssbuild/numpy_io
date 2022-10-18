e0b26b718aea6fbc2b7f646cb35a4f6a7da758f6
ssbuild <462304@qq.cn>
2022-10-18 09:55:26 +0800

    2022年10月18日09:55:22
    Signed-off-by: ssbuild <462304@qq.cn>

--- reader_example.py
+++ reader_example.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2022/10/18 9:36
+
 from fastdatasets import TFRecordOptions,RecordLoader,FeatrueWriter,DataType,gfile
 
 def read_data(record_filenames,compression_type='GZIP'):
     options = TFRecordOptions(compression_type=compression_type)
     dataset_reader = RecordLoader.IterableDataset(record_filenames, options=options, with_share_memory=True)
 
+    i = 0
+    for example in dataset_reader:
+        if i % 1000 == 0:
+            print(example)
+        i += 1
+
+    # dataset 变换操作
+    dataset_reader = dataset_reader.apply(lambda x:x).repeat(2).shuffle(128)
     i = 0
     for example in dataset_reader:
         if i % 1000 == 0:
@@ -20,3 +29,11 @@ def read_data2(record_filenames,compression_type='GZIP'):
         example = dataset_reader[i]
         if i % 1000 == 0:
             print(example)
+
+    # dataset 变换操作
+    dataset_reader = dataset_reader.apply(lambda x: x).repeat(2).shuffle(128)
+    i = 0
+    for example in dataset_reader:
+        if i % 1000 == 0:
+            print(example)
+        i += 1
