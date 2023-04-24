930c997b718022199d396a46aec4872d5b890317
ssbuild <462304@qq.cn>
2023-04-24 09:31:43 +0800

    初始化numpy-io
    Signed-off-by: ssbuild <462304@qq.cn>

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/24 9:15
+
+from setuptools import setup, find_packages
+
+ignore = ['test','tests']
+setup(
+    name='numpy-io',
+    version='0.0.1rc',
+    description='an easy training architecture',
+    long_description='numpy-io: https://github.com/ssbuild/numpy-io.git',
+    license='Apache License 2.0',
+    url='https://github.com/ssbuild/numpy-io',
+    author='ssbuild',
+    author_email='9727464@qq.com',
+    install_requires=[
+                      'fastdatasets>=0.9.6 , <= 1',
+                      'numpy',
+                      'tqdm'
+    ],
+    packages=[p for p in find_packages() if p not in ignore]
+)
